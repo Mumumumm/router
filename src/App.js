4010604647da93cbba489e2969a823c193c6b89d
김준홍 <rlawnsghd434343@gmail.com>
2025-03-28 12:08:07 +0900

    first file href 경로넣어보기

--- src/App.js
+++ src/App.js
@@ -4,20 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello React Router</h1>
+      <ul>
+        <li><a href='/home'>Home</a></li>
+        <li><a href='/topics'>Topics</a></li>
+        <li><a href='/contact'>Contact</a></li>
+      </ul>
     </div>
   );
 }
